9900ce3a9aa16cf111bd9e12007ed60978096081
Shubham Sahu <shubham30sahu101@gmail.com>
2025-08-10 00:02:29 +0530

    initial

---
+++ dataProducer/dataProducer/src/test/java/com/driveGuard/dataProducer/DataProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driveGuard.dataProducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
