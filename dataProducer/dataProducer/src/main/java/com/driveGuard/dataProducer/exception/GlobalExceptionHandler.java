9a6c1c8c416e679ef7bd4856fd53a8bba9d7d82d
Shubham Sahu <shubham30sahu101@gmail.com>
2026-01-04 03:38:44 +0530

    Error handling for wrong parameters

--- dataProducer/dataProducer/src/main/java/com/driveGuard/dataProducer/exception/GlobalExceptionHandler.java
+++ dataProducer/dataProducer/src/main/java/com/driveGuard/dataProducer/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.driveGuard.dataProducer.exception;
 
+import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,6 +12,13 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(TripNotFoundException.class)
     public ResponseEntity<String> handleTripNotFoundException(TripNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    // For wrong URL parameters (Spring Data Error)
+    @ExceptionHandler(PropertyReferenceException.class)
+    public ResponseEntity<String> handlePropertyReferenceException(PropertyReferenceException ex) {
+        String message = "Invalid sort parameter: '" + ex.getPropertyName() + "'. Please check for typos or extra spaces.";
+        return new ResponseEntity<>(message, HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
